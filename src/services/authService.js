de6e74da1273f8420b421ecae5fef2190a86c612
kayhan-kashi <Kayhan.kashi@gmail.com>
2024-12-07 17:19:57 +0330

    toast done

---
+++ src/services/authService.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const API_BASE_URL = "https://jsonplaceholder.typicode.com";
+
+
+const register = async(userData) => {
+    // return await axios.post(`${API_BASE_URL}/posts`, userData);
+    return await axios.post(`${API_BASE_URL}/posts`, {
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+    });
+}
+
+const login = async(userData) => {
+    // return await axios.post(`${API_BASE_URL}/posts`, userData);
+    return await axios.post(`${API_BASE_URL}/posts`, {
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+    });
+}
+
+export const registerUser = register;
+export const loginUser = login;
