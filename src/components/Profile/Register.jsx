23f4cdd7d7810e2f642a4c3087953e9393c79ac4
Kayhan-Kashi <Kayhan.Kashi@gmail.com>
2024-12-12 14:23:33 +0330

    userInfo responsive

--- src/components/Profile/Register.jsx
+++ src/components/Profile/Register.jsx
@@ -17,10 +17,6 @@ const registrationSchema = z
       .string()
       .min(3, "نام کاربری باید حداقل ۳ کاراکتر باشد")
       .nonempty("ورود نام کاربری الزامی است"),
-    // email: z
-    //   .string()
-    //   .email("ایمیل باید مجاز باشد")
-    //   .nonempty("ورود ایمیل الزامی است"),
     password: z.string().min(6, "رمز عبور باید حداقل ۶ کاراکتر باشد"),
     confirmPassword: z.string(),
   })
@@ -47,15 +43,22 @@ const Register = () => {
 
   const submitUser = (formData) => {
     setLoading(true);
-    validateUsername({ username: formData.username, password: formData.password })
+    validateUsername({
+      username: formData.username,
+      password: formData.password,
+    })
       .then((response) => {
         toast.success("نام کاربری و رمز عبور قابل قبول است", {
           position: "top-center",
           autoClose: 500,
           style: { fontSize: "14px", padding: "10px" },
           onClose: () => {
-            navigate("/userInfo",
-               { state: { username: formData.username, password: formData.password}});
+            navigate("/userInfo", {
+              state: {
+                username: formData.username,
+                password: formData.password,
+              },
+            });
             setLoading(true);
           },
         });
@@ -92,7 +95,7 @@ const Register = () => {
           style={{ backgroundImage: `url(${image})` }}
         >
           <ToastContainer />
-          <div className="w-1/2 bg-slate-800 border border-slate-400 rounded-md p-20 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-50 relative">
+          <div className="w-3/5 bg-slate-800 border border-slate-400 rounded-md md:p-20 sm:shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative ">
             <div>
               <h1 className="text-4xl font-bold text-white mb-6 text-center">
                 ثبت نام
@@ -102,57 +105,40 @@ const Register = () => {
                 className="flex flex-col"
                 onSubmit={handleSubmit(submitUser)}
               >
-                <div className="my-4 relative flex">
-                  <label className={`flex-[1_1_20%] text-white text-lg`}>
-                    نام کاربری :
+                <div className="flex flex-col lg:flex-row my-4 relative">
+                  <label
+                    className={`flex-[1_1_30%] text-white sm:text-sm md:text-lg text-center lg:text-right pr-5 whitespace-nowrap flex-shrink-0`}
+                  >
+                    نام کاربری <span className={"hidden lg:inline"}>:</span>
                   </label>
                   <input
                     {...register("username")}
                     type="text"
-                    className={`flex-[4_1_80%] py-2.3 px-2 
-                      text-xl text-white font-bold
-                      bg-transparent border-0 border-b-2 border-gray-300 
-                      appearance-none dark:focus:border-blue-500
-                      focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer `}
+                    className={`flex-[1_1_50%] py-2.3 px-2 
+                    text-sm sm:text-xl text-white font-bold
+                    bg-transparent border-0 border-b-2 border-gray-300 
+                    appearance-none dark:focus:border-blue-500 
+                    focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer`}
                   />
+                  <div className="flex-[4_1_20%]"></div>
                 </div>
                 {errors.username && (
                   <p className="text-red-500 text-lg text-center">
                     {errors.username.message}
                   </p>
                 )}
-                {/* <div className="my-4 relative flex">
+
+                <div className="flex flex-col lg:flex-row my-4 relative">
                   <label
-                    className={`text-white flex-[1_1_20%] text-lg text-left`}
+                    className={`flex-[1_1_30%] text-white sm:text-sm md:text-lg text-center lg:text-right pr-5 whitespace-nowrap flex-shrink-0`}
                   >
-                    ایمیل :
-                  </label>
-                  <input
-                    type="email"
-                    {...register("email")}
-                    className={`flex-[4_1_80%] py-2.3 px-2 
-                      text-xl text-white font-bold
-                      bg-transparent border-0 border-b-2 border-gray-300 
-                      appearance-none dark:focus:border-blue-500 
-                      focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer`}
-                  />
-                </div>
-                {errors.email && (
-                  <p className="text-red-600 text-lg text-center">
-                    {errors.email.message}
-                  </p>
-                )} */}
-                <div className="my-4 relative flex">
-                  <label
-                    className={`text-white flex-[1_1_30%] text-lg text-left`}
-                  >
-                    رمز عبور :
+                    رمز عبور <span className={"hidden lg:inline"}>:</span>
                   </label>
                   <input
                     type={passToggle ? "text" : "password"}
                     {...register("password")}
-                    className={`flex-[4_1_55%] py-2.3 px-2 
-                      text-xl text-white font-bold
+                    className={`flex-[4_1_50%] py-2.3 px-2 
+                      text-sm sm:text-xl text-white font-bold
                       bg-transparent border-0 border-b-2 border-gray-300 
                       appearance-none dark:focus:border-blue-500 
                       focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer`}
@@ -162,29 +148,29 @@ const Register = () => {
                   <button
                     type="button"
                     onClick={togglePassword}
-                    className="absolute top-3 right-4 focus:outline-none text-white"
+                    className="hidden lg:block absolute top-3 sm:right-20 focus:outline-none text-white "
                   >
                     {passToggle ? <FaRegEyeSlash /> : <FaRegEye />}
                   </button>
-                  <div className="flex-[4_1_15%]"></div>
+                  <div className="flex-[4_1_20%]"></div>
                 </div>
                 {errors.password && (
                   <p className="text-red-600 text-lg text-center">
                     {errors.password.message}
                   </p>
                 )}
-                <div className="flex my-4 relative">
+                <div className="flex flex-col lg:flex-row my-4 relative">
                   <label
-                    className={`flex-[1_1_30%] text-white text-lg text-left`}
+                    className={`flex-[1_1_30%] text-white sm:text-sm md:text-lg text-center lg:text-right pr-5 whitespace-nowrap flex-shrink-0`}
                   >
-                    تکرار رمز عبور :
+                    تکرار رمز عبور <span className={"hidden lg:inline"}>:</span>
                   </label>
                   <input
                     type={cnfrmPassToggle ? "text" : "password"}
                     {...register("confirmPassword")}
-                    className={`flex-[4_1_55%] py-2.3 px-2 
-                      text-xl text-white font-bold 
-                      bg-transparent border-b-2 border-gray-300 
+                    className={`flex-[4_1_50%] py-2.3 px-2 
+                      text-sm sm:text-xl text-white font-bold
+                      bg-transparent border-0 border-b-2 border-gray-300 
                       appearance-none dark:focus:border-blue-500 
                       focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer`}
                     placeholder=""
@@ -192,11 +178,11 @@ const Register = () => {
                   <button
                     type="button"
                     onClick={cnfrmTogglePassword}
-                    className="absolute top-3 right-4 focus:outline-none text-white"
+                    className="hidden lg:block pl-2 absolute top-3 sm:right-20 focus:outline-none text-white "
                   >
                     {cnfrmPassToggle ? <FaRegEyeSlash /> : <FaRegEye />}
                   </button>
-                  <div className="flex-[4_1_15%]"></div>
+                  <div className="flex-[4_1_20%]"></div>
                 </div>
                 {errors.confirmPassword && (
                   <p className="text-red-600 text-lg text-center">
@@ -220,7 +206,7 @@ const Register = () => {
                      bg-white text-emerald-800
                       hover:bg-amber-600 hover:text-white py-2 transition-colors duration-300`}
                 >
-                {loading ? "... در حال بررسی " : "ثبت نام"}
+                  {loading ? "... در حال بررسی " : "ثبت نام"}
                 </button>
               </form>
             </div>
--- src/components/Profile/UserInfoForm.jsx
+++ src/components/Profile/UserInfoForm.jsx
@@ -45,7 +45,7 @@ const UserInfoForm = () => {
       grade: formData.grade,
       schoolName: formData.schoolName,
       username: username,
-      password: password
+      password: password,
     })
       .then((response) => {
         toast.success("ثبت نام با موفقیت انجام شد", {
@@ -89,7 +89,7 @@ const UserInfoForm = () => {
           style={{ backgroundImage: `url(${image})` }}
         >
           <ToastContainer />
-          <div className="w-2/3 bg-slate-800 border border-slate-400 rounded-md p-20 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-50 relative">
+          <div className="w-2/3 bg-slate-800 border border-slate-400 rounded-md p-24 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-50 relative">
             <div>
               <h1 className="text-4xl font-bold text-white mb-6 text-center">
                 ورود اطلاعات
@@ -99,50 +99,66 @@ const UserInfoForm = () => {
                 className="flex flex-col"
                 onSubmit={handleSubmit(finalizeRegistration)}
               >
-                <div className="flex ">
-                  <div className="flex w-1/2 my-4 relative">
+                <div className="flex flex-col lg:flex-row-reverse justify-items-center">
+                  <div className="flex flex-col lg:flex-row-reverse my-4 relative ">
+                    <label
+                      className={`flex-[1_1_20%] text-white sm:text-sm md:text-lg text-center lg:text-right pr-5 whitespace-nowrap flex-shrink-0`}
+                    >
+                      <span className={"hidden lg:inline"}>:</span> نام
+                    </label>
+                    <div className={`flex flex-row`}>
+                      <input
+                        {...register("firstName")}
+                        type="text"
+                        className={`flex-[1_1_50%] py-2.3 px-2 
+                        text-sm sm:text-xl text-white font-bold text-center
+                        bg-transparent border-0 border-b-2 border-gray-300 
+                        appearance-none dark:focus:border-blue-500 
+                        focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer lg:ml-10`}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col lg:flex-row-reverse my-4 relative lg:pr-8">
+                    <label
+                      className={`flex-[1_1_35%] text-white sm:text-sm md:text-lg text-center lg:text-right pr-5 whitespace-nowrap flex-shrink-0`}
+                    >
+                      <span className={"hidden lg:inline"}>:</span> نام خانوادگی
+                    </label>
                     <input
                       {...register("lastName")}
                       type="text"
-                      className={`flex-[4_1_80%] py-2.3 px-4
-                      text-xl text-white font-bold text-center
-                      bg-transparent border-0 border-b-2 border-gray-300 
-                      appearance-none dark:focus:border-blue-500
-                      focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer `}
+                      className={`flex-[1_1_50%] py-2.3 px-2 
+                        text-sm sm:text-xl text-white font-bold text-center
+                        bg-transparent border-0 border-b-2 border-gray-300 
+                        appearance-none dark:focus:border-blue-500 
+                        focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer `}
                     />
-                    <label className={`flex-[1_1_40%] text-white text-lg`}>
-                      : نام خانوادگی
-                    </label>
                   </div>
-                  <div className="flex w-1/2 my-4 relative">
+                </div>
+                <div className="flex flex-col lg:flex-row-reverse justify-items-center">
+                  <div className="flex flex-col lg:flex-row-reverse my-4 relative">
+                    <label
+                      className={`flex-[1_1_20%] text-white sm:text-sm md:text-lg text-center lg:text-right pr-5 whitespace-nowrap flex-shrink-0`}
+                    >
+                      <span className={"hidden lg:inline"}>:</span> کد ملی
+                    </label>
                     <input
-                      {...register("firstName")}
+                      {...register("nationalCode")}
                       type="text"
-                      className={`flex-[4_1_60%] py-2.3 px-2 
-                      text-xl text-white font-bold text-center
-                      bg-transparent border-0 border-b-2 border-gray-300 
-                      appearance-none dark:focus:border-blue-500
-                      focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer `}
+                      className={`flex-[1_1_50%] py-2.3 px-2 
+                        text-sm sm:text-xl text-white font-bold text-center
+                        bg-transparent border-0 border-b-2 border-gray-300 
+                        appearance-none dark:focus:border-blue-500 
+                        focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer lg:ml-10`}
                     />
-                    <label className={`flex-[1_1_10%] text-white text-lg`}>
-                      : نام
-                    </label>
                   </div>
-                </div>
-                <div className="flex">
-                  {errors.lastName && (
-                    <p className="text-red-500 text-lg text-center w-1/2">
-                      {errors.lastName.message}
-                    </p>
-                  )}
-                  {errors.firstName && (
-                    <p className="text-red-500 text-lg text-center w-1/2">
-                      {errors.firstName.message}
-                    </p>
-                  )}
-                </div>
-                <div className="flex ">
-                  <div className="flex w-1/2 my-4 relative text-center">
+                  <div className="flex flex-col lg:flex-row-reverse my-4 relative ">
+                    <label
+                      className={`flex-[1_1_3۰%] text-white sm:text-sm md:text-lg text-center lg:text-right pr-5 whitespace-nowrap flex-shrink-0`}
+                    >
+                      <span className={"hidden lg:inline"}>:</span> پایه تحصیلی
+                    </label>
                     <select
                       className={`rounded-full flex-[1_1_40%] text-center border-2 border-black`}
                       {...register("grade")}
@@ -156,67 +172,33 @@ const UserInfoForm = () => {
                       <option value={2}> ششم تا دهم</option>
                       <option value={3}> یازدهم تا دوازدهم </option>
                     </select>
-                    <label className={`flex-[1_1_10%] text-white text-lg`}>
-                      :پایه تحصیلی
-                    </label>
-                  </div>
-                  <div className="flex w-1/2 my-4 relative">
-                    <input
-                      {...register("nationalCode")}
-                      type="text"
-                      className={`flex-[4_1_80%] py-2.3 px-2 
-                      text-xl text-white font-bold text-center
-                      bg-transparent border-0 border-b-2 border-gray-300 
-                      appearance-none dark:focus:border-blue-500
-                      focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer `}
-                    />
-                    <label className={`flex-[1_1_30%] text-white text-lg`}>
-                      :شماره ملی
-                    </label>
                   </div>
                 </div>
-                <div className="flex">
-                  {errors.grade && (
-                    <p className="text-red-500 text-lg text-center w-1/2">
-                      {errors.grade.message}
-                    </p>
-                  )}
-                  {errors.nationalCode && (
-                    <p className="text-red-500 text-lg text-center w-1/2">
-                      {errors.nationalCode.message}
-                    </p>
-                  )}
-                </div>
-
-                <div className="flex">
-                  <div className="flex w-2/3 my-4 relative mx-auto">
+                <div className="flex flex-col lg:flex-row-reverse justify-items-center">
+                  <div className="flex flex-col lg:flex-row-reverse my-4 relative">
+                    <label
+                      className={`flex-[1_1_20%] text-white sm:text-sm md:text-lg text-center lg:text-right pr-5 whitespace-nowrap flex-shrink-0`}
+                    >
+                      <span className={"hidden lg:inline"}>:</span> نام مدرسه
+                    </label>
                     <input
                       {...register("schoolName")}
                       type="text"
-                      className={`flex-[4_1_80%] py-2.3 px-2 
-                      text-xl text-white font-bold text-center
-                      bg-transparent border-0 border-b-2 border-gray-300 
-                      appearance-none dark:focus:border-blue-500
-                      focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer `}
+                      className={`flex-[1_1_50%] py-2.3 px-2 
+                        text-sm sm:text-xl text-white font-bold text-center
+                        bg-transparent border-0 border-b-2 border-gray-300 
+                        appearance-none dark:focus:border-blue-500 
+                        focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer lg:ml-10`}
                     />
-                    <label className={`flex-[1_1_30%] text-white text-lg`}>
-                      : نام مدرسه
-                    </label>
                   </div>
                 </div>
-                {errors.schoolName && (
-                  <p className="text-red-500 text-lg text-center">
-                    {errors.schoolName.message}
-                  </p>
-                )}
-
                 <button
                   type="submit"
                   className={`block mx-auto mb-4 text-[18px] mt-6 rounded-full text-center w-4/5
                      bg-white text-emerald-800
                       hover:bg-amber-600 hover:text-white py-2 transition-colors duration-300`}
                 >
-                {loading ? "... در حال بررسی " : "تکمیل ثبت نام"}
+                  {loading ? "... در حال بررسی " : "تکمیل ثبت نام"}
                 </button>
               </form>
             </div>
